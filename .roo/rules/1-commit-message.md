a6f71f38031ecc4f41bafdf1c1e9a48a1ed27849
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2025-08-01 22:35:12 +0200

    feat: add HelloWorldController with basic endpoint
    - Create HelloWorldController.java with /hello endpoint
    
    - Add .roo configuration files
    
    - Add compose.yaml for containerization

---
+++ .roo/rules/1-commit-message.md
@@ -0,0 +1,4 @@
+# Commit
+
+- I prefer using conventional commit for my commit messages.
+- I prefer adding a body in list with small points.
---
+++ src/main/java/gis/adorsys/demo/ssapp/HelloWorldController.java
@@ -0,0 +1,13 @@
+package gis.adorsys.demo.ssapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "hello world";
+    }
+}
